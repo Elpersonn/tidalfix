13941cb4903e4a647de88201584e0c9ca794079c
Elperson <secret@a.com>
2022-09-17 19:18:55 +0200

    utf-8

--- prepare.js
+++ prepare.js
@@ -3,6 +3,6 @@ const require = createRequire(import.meta.url);
 const fs = require('node:fs')
 const Database = require('better-sqlite3')
 const database = new Database('tidalfix.db')
-database.exec(fs.readFileSync('dump.sql'))
+database.exec(fs.readFileSync('dump.sql', 'utf-8'))
 database.close()
 console.log('database ready')
