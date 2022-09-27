c0638fa99072126a882674e072cdad611762655b
Elperson <secret@a.com>
2022-09-27 19:24:22 +0200

    Homepage changes

--- static/index.js
+++ static/index.js
@@ -3,16 +3,8 @@ marked.setOptions({
     baseUrl: "tidalfix.com"
 })
 document.getElementById("guide").innerHTML = marked.parse(`# **TIDALFIX USAGE GUIDE**
-
-## Albums / Singles
 <ol>
     <li>Get any TIDAL link e.g.:https://tidal.com/browse/album/148995697</li>
     <li>Replace tidal.com with tidalfix.com</li>
     <li>Post it on discord and watch the embed show up!</li>
-</ol>
-
-## Artists
-Basically the same process
-
-## Tracks
-Basically the same process`)
+</ol>`)
