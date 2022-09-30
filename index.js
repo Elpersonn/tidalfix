6bed5d5833e21e795accbb71d7ee8f0b9daeb19b
Elperson <secret@a.com>
2022-09-30 17:50:19 +0200

    Reduced code size
    Made the routes more dynamic hence reducing size

--- index.js
+++ index.js
@@ -23,19 +23,16 @@ const crypt = require("node:crypto")
 const http = require('http')
 const puppeteer = require('puppeteer')
 const teplates = require('sprightly')
-
 import {secret, port} from './config.mjs'
 import { exec } from 'node:child_process'
 import { pid } from 'node:process'
 
-
-
 //const Tidal = require('tidalapi')
 const db = require('better-sqlite3')('tidalfix.db')
-const getid = db.prepare("SELECT * FROM embeds WHERE ID = ?")
+const getid = db.prepare("SELECT * FROM ALBUMS WHERE ID = ?")
 const insertbad = db.prepare("INSERT INTO blacklisted (ID) VALUES (?)")
 const findbad = db.prepare("SELECT * FROM blacklisted WHERE ID = ?")
-const insertfound = db.prepare("INSERT INTO embeds (id, created, title, description, image) VALUES (?, strftime('%s', 'now'), ?, ?, ?)")
+const insertfound = db.prepare("INSERT INTO ALBUMS (id, created, title, description, image) VALUES (?, strftime('%s', 'now'), ?, ?, ?)")
 const getartist = db.prepare("SELECT * FROM artists WHERE id = ?")
 const insertartist = db.prepare("INSERT OR IGNORE INTO ARTISTS (id, created, name, description, image) VALUES (?, strftime('%s', 'now'), ?, ?, ?)")
 const findbadartist = db.prepare("SELECT * FROM artist_blacklist WHERE ID = ?")
@@ -52,7 +49,6 @@ const app = express()
 app.engine('spy', teplates)
 app.set('views', './')
 app.set('view engine', 'spy')
-
 
 const botRegex = new RegExp(/Discordbot\/2\.0|Twitterbot\/1.0/)
 const reqoptns = {
@@ -100,11 +96,8 @@ async function Scrape(url) {
     }
     return page
 }
-
 app.use(express.static('static'))
-app.use("(/browse)?/album/:id", ValidateId)
-app.use("(/browse)?/artist/:id", ValidateId)
-app.use("(/browse)?/track/:id", ValidateId)
+app.use("(/browse)?/:any/:id", ValidateId)
 app.use("(/browse)?/album/:id", ValidateBlacklist)
 app.use("(/browse)?/artist/:id", async (req, res, next) => {
     if (req.params.id >= 250) { // 250 is the lowest artist id
@@ -123,20 +116,13 @@ app.use("(/browse)?/track/:id", async (req, res, next) => {
     else
         res.status(406)
 })
-app.get('/browse/album/:id/json', async (req, res) => {
+app.get('(/browse)?/:empath/:id/json', async (req, res) => {
     res.json({
         "provider_name": "Listen on TIDAL",
-        "provider_url": `https://tidal.com/browse/album/${req.params.id}`,
+        "provider_url": `https://tidal.com/browse/${req.params.empath}/${req.params.id}`,
     })
     
 })
-app.get('/browse/artist/:id/json', async (req, res) => {
-    res.json({
-        provider_name: 'Listen on TIDAL',
-        provider_url: `https://tidal.com/browse/artist/${req.params.id}`
-    })
-})
-
 
 app.post('/gitPush', async (req, res) => {
     let secure = req.get('x-hub-signature-256')
@@ -156,21 +142,23 @@ app.post('/gitPush', async (req, res) => {
 })
 // TODO: DO THE UNDERMENTIONED
 // all the browse code could be boiled down to a single thing tbf
-app.get('(/browse)?/track/:id', async (req, res) => {
-    console.log("TRACK REQUESTED")
-    let dbres = await findtrack.get(req.params.id)
+app.get('(/browse)?/:type/:id', async (req, res) => {
+    if (req.params.type.toLowerCase() != ("album" || "track" || "artist")) {
+        res.status(400).send("Wrong type")
+    }//                                               vvvvvvvvvvvvvvvvvvvvvvvvvvvvv - Yes, this DOES have the possibility of an SQL injection but not when I literally sanitized it and checked it
+    let dbres = await db.prepare(`SELECT * FROM ${req.params.type.toLowerCase() + "s"} WHERE id = ?`).get(req.params.id)
     if (dbres && dbres.title) {
         if (botRegex.test(req.get('User-Agent'))) {
-            res.render('standard.spy', {title: dbres.title, description: dbres.description, image: dbres.image })
+            res.render('standard.spy', {title: dbres.title, description: dbres.description, image: dbres.image, request: req.params.id  })
         }else {
-            res.redirect(`https://tidal.com/browse/track/${req.params.id}`)
+            res.redirect(`https://tidal.com/browse/${req.params.type}/${req.params.id}`)
         }
     }else{
-        console.log("SCRAPING TRACK")
-        let sres = await Scrape(`https://tidal.com/browse/track/${req.params.id}`)
-        if (sres && sres.title) {
-            await inserttrack.run(req.params.id, sres.title, sres.description, sres.image)
-            res.redirect(`/browse/track/${req.params.id}`)
+        console.log("SCRAPING")
+        let sres = await Scrape(`https://tidal.com/browse/${req.params.type}/${req.params.id}`)
+        if (sres && sres.title) {//             vvvvvvvvvvvvvvvvvvvvvvvvvvvvv - line 154
+            await db.prepare(`INSERT INTO ${req.params.type.toLowerCase() + "s"} (id, created, ${(req.params.type == 'artist') ? "name" : "title"}, description, image) VALUES (?, strftime('%s', 'now'), ?, ?, ?)`).run(req.params.id, sres.title, sres.description, sres.image)
+            res.redirect(`/browse/${req.params.type}/${req.params.id}`)  //                                              ^ plural :)
         }else{
             switch (sres) {
                 case 404:
@@ -187,86 +175,6 @@ app.get('(/browse)?/track/:id', async (req, res) => {
         }
     }
 })
-
-app.get('(/browse)?/artist/:id', async (req, res) => {
-    console.log("ARTIST REQUESTED")
-    let dbres = await getartist.get(req.params.id)
-    
-    if (dbres && dbres.name) {
-        if (botRegex.test(req.get("User-Agent"))) {
-            res.render('standard.spy', {title: dbres.name, description: dbres.description, image: dbres.image })
-
-        } else {
-            res.redirect(`https://tidal.com/browse/artist/${req.params.id}`)
-        }
-    }else{
-        console.log("SCRAPING ARTIST")
-        let sres = await Scrape(`https://tidal.com/browse/artist/${req.params.id}`)
-        if (sres && sres.title) {
-            await insertartist.run(req.params.id, sres.title, sres.description, sres.image)
-            res.redirect(`/browse/artist/${req.params.id}`)
-        }else{
-            switch (sres) {
-                case 404:
-                    await insertbadart.run(req.params.id)
-                    res.status(404).send("NOT FOUND")
-                    break;
-                case 403:
-                    res.status(500).send("REQUEST BLOCKED BY TIDAL<br>PLEASE TRY AGAIN LATER")
-                    break;
-                default:
-                    res.status(500).send(`unknown ${sres}`)
-                    break;
-            }
-        }
-    }
-})
-
-// TODO: use middleware for both
-
-
-app.get('(/browse)?/album/:id', async (req, res) => {
-    console.log("ALBUM REQUESTED")
-    let dbres = await getid.get(req.params.id)
-    if (dbres && dbres.title && dbres.description && dbres.image) {
-        console.log("SERVING FROM CACHE")
-        if (botRegex.test(req.get("User-Agent"))) {
-            res.render('standard.spy', {title: dbres.title, description: dbres.description, image: dbres.image })
-
-        } else {
-            res.redirect(`https://tidal.com/browse/album/${dbres.id}`)
-        }
-    } else {
-        console.log("SCRAPING ALBUM/SINGLE")
-        let sres = await Scrape(`https://tidal.com/browse/album/${req.params.id}`)
-        if (sres && sres.title) {
-            insertfound.run(req.params.id, sres.title, sres.description, sres.image)
-            res.redirect(`/browse/album/${req.params.id}`)
-        }else{
-            switch (sres) {
-                case 404:
-                    await insertbad.run(req.params.id)
-                    res.status(404).send("NOT FOUND")
-                    break;
-                case 403:
-                    res.status(500).send("REQUEST BLOCKED BY TIDAL<br>PLEASE TRY AGAIN LATER")
-                    break;
-            }
-        }
-    }
-
-
-
-
-    /*res.send(`<head>
-        <meta property="og:title" content="TEST" />
-        <meta property="og:author" content="Elperson" />
-        <meta property="og:description" content="embed test" />
-        <meta property="og:image" content="https://resources.tidal.com/images/5b22b4ad/2358/4418/acae/2a2c226e5945/1280x1280.jpg" />
-    
-    </head>`)*/
-})
-
 app.listen(port, '0.0.0.0', () => {
     console.log("Starting server...")
     console.warn(`Running on port ${port}`)
